de2e0edd13b06086769c55c778291138123846a5
T.J. McCarty <pagecow@gmail.com>
2023-06-05 06:41:01 -0500

    update

---
+++ app/hello/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [time, setTime] = useState('');
+  
+  useEffect(() => {
+    getTime()
+  }, [])
+
+  async function getTime() {
+    const res = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago')
+    const data = await res.json()
+    console.log("data: ", data.datetime)
+
+    setTime(data.datetime)
+  }
+
+  return( 
+    <div className='bg-white h-screen w-full p-10'>
+      <h1 className='m-10'>{time}</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-type Time = {
-  datetime: string
-}
-
-async function getTime(): Promise<Time> {
-  const res = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago')
-
-  return res.json()
-}
-
-export default async function Home() {
-  const [time] = await Promise.all([getTime()])
-
+export default function Home() {
   return( 
     <div className='bg-white h-screen w-full p-10'>
-      <h1 className='m-10'>{time.datetime}</h1>
+      <a href="/hello" className='m-10'>Go to Hello page</a>
     </div>
   )
 }
-
-export const revalidate = 3;
